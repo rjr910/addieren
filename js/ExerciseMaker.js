fbc3e08b8b15f3f1aa8cdeb1f21e6194386532f9
rjr910 <rjr910@googlemail.com>
2016-12-18 21:10:28 +0100

    updated some code style; added checkboxes to choose between operators

--- js/ExerciseMaker.js
+++ js/ExerciseMaker.js
@@ -2,34 +2,49 @@
  * Created by rjr910 on 05.11.16.
  */
 
-'use strict';
+
 
 var ExcerciseMaker = function () {
-    var maxNumber = $('#maxValue').val();
-    var number1 = genNumber(maxNumber);
-    var number2 = genNumber(maxNumber);
+    "use strict";
+
+    var maxNumber = $("#maxValue").val();
+    var number1;
+    var number2;
+    var operator;
     var $eingabe = $("#eingabe");
     var $zahl1 = $("#zahl1");
     var $zahl2 = $("#zahl2");
+    var $operator = $("#operator");
     var $progress = $("#progress");
     var $richtige = $("#made");
     var countExcercise = $("#untilVideo");
+    var plus = $("#plus");
+    var minus = $("#minus");
+    var times = $("#times");
+    var operatorArr = ["+"];
     var vk = new VideoKeeper();
 
 
-    vk.addVideo('<iframe  src="https://www.youtube.com/embed/EIiBbUkRckE?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>');
-    vk.addVideo('<iframe  src="https://www.youtube.com/embed/Q_VpkXd1TKA?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-    vk.addVideo('<iframe  src="https://www.youtube.com/embed/eo6i32yWbOY?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-    vk.addVideo('<iframe  src="https://www.youtube.com/embed/pW2bMzgiWpA?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-    vk.addVideo('<iframe  src="https://www.youtube.com/embed/21OqT-tbv_w?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-    vk.addVideo('<iframe  src="https://www.youtube.com/embed/x4xrOY5LKM8?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-    vk.addVideo('<iframe  src="https://www.youtube.com/embed/BjwDV1Is34U?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-    vk.addVideo('<iframe  src="https://www.youtube.com/embed/n-1YjVy2ZTs?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+    vk.addVideo("<iframe  src=\"https://www.youtube.com/embed/EIiBbUkRckE?rel=0&autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>");
+    vk.addVideo("<iframe  src=\"https://www.youtube.com/embed/Q_VpkXd1TKA?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>");
+    vk.addVideo("<iframe  src=\"https://www.youtube.com/embed/eo6i32yWbOY?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>");
+    vk.addVideo("<iframe  src=\"https://www.youtube.com/embed/pW2bMzgiWpA?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>");
+    vk.addVideo("<iframe  src=\"https://www.youtube.com/embed/21OqT-tbv_w?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>");
+    vk.addVideo("<iframe  src=\"https://www.youtube.com/embed/x4xrOY5LKM8?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>");
+    vk.addVideo("<iframe  src=\"https://www.youtube.com/embed/BjwDV1Is34U?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>");
+    vk.addVideo("<iframe  src=\"https://www.youtube.com/embed/n-1YjVy2ZTs?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>");
 
     var createExcercise = function () {
+        operator = operatorArr[genNumber(operatorArr.length - 1)];
         number1 = genNumber(maxNumber);
         number2 = genNumber(maxNumber);
-
+        if (operator === "-") {
+            if (number1 < number2) {
+                var tmp = number1;
+                number1 = number2;
+                number2 = tmp;
+            }
+        }
     };
 
     this.setMaxNumber = function (n) {
@@ -40,7 +55,17 @@ var ExcerciseMaker = function () {
         createExcercise();
         $zahl1.val(number1);
         $zahl2.val(number2);
+        $operator.html(operator);
         $eingabe.val(null);
+    };
+
+    this.addOperators = function (op) {
+        operatorArr.push(op);
+    };
+
+    this.removeOperator = function (op) {
+        var index = operatorArr.indexOf(op);
+        operatorArr.slice(index, 1);
     };
 
     this.setCountExercise = function (n) {
@@ -53,12 +78,24 @@ var ExcerciseMaker = function () {
     };
 
     this.doCheck = function () {
-
+        var erge;
         if ($eingabe.val()) {
-            var erge = number1 + number2;
-            if (erge == $eingabe.val()) {
-                $richtige.append("<div style='display: none'>" + $zahl1.val() + "+" + $zahl2.val() + "=" + erge + "</div>");
-                $('#made div').fadeIn('slow');
+            switch (operator) {
+                case "+":
+                    erge = number1 + number2;
+                    break;
+                case "-":
+                    erge = number1 - number2;
+                    break;
+                case "x":
+                    erge = number1 * number2;
+                    break;
+            }
+
+            if (erge === Number($eingabe.val())) {
+                $richtige.append("<div style='display: none'>" + $zahl1.val() + " " + operator + " " + $zahl2.val() +
+                    " = " + erge + "</div>");
+                $("#made div").fadeIn("slow");
                 $progress.val($progress.val() + 100 / countExcercise.val());
                 if (Number($progress.val()) >= 100) {
                     $(".aufgabe").hide();
@@ -67,9 +104,9 @@ var ExcerciseMaker = function () {
 
 
                     var videos = vk.getVideos();
-                    $('#video').css({
-                        width: $(window).innerWidth() + 'px',
-                        height: Number($(window).innerHeight() - 50) + 'px'
+                    $("#video").css({
+                        width: $(window).innerWidth() + "px",
+                        height: Number($(window).innerHeight() - 50) + "px"
                     });
                     $("#video").append(videos[genNumber(videos.length)]);
                 }
--- js/script.js
+++ js/script.js
@@ -2,10 +2,8 @@
  * Created by rjr910 on 28.10.2016.
  */
 
-
-'use strict';
-
 function genNumber(zahl) {
+    "use strict";
     return Math.round(Math.random() * zahl);
 }
 
@@ -15,23 +13,58 @@ $("#eingabe").focus();
 
 
 $("#eingabe").on("input", function () {
+    "use strict";
     em.doCheck();
 });
 
 $('#maxValue').on("change", function () {
+    "use strict";
     em.setMaxNumber(this.value);
     em.fillNumberFields();
 });
 
 $('#untilVideo').on("change", function () {
+    "use strict";
     em.setCountExercise(this.value);
     em.fillNumberFields();
 });
 
 $(window).resize(function () {
+    "use strict";
     $('#video').css({width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px'});
 });
 
-$('#reset').on('click', function () {
+$("#reset").on("click", function () {
+    "use strict";
     location.reload();
+});
+
+$("#plus").on("click", function () {
+    "use strict";
+    if ($("#plus").prop("checked")) {
+        em.addOperators("+");
+    } else {
+        em.removeOperator("+");
+    }
+    em.fillNumberFields();
+});
+
+$("#minus").on("click", function () {
+    "use strict";
+    if ($("#minus").prop("checked")) {
+        em.addOperators("-");
+    } else {
+        em.removeOperator("-");
+    }
+    em.fillNumberFields();
+});
+
+$("#times").on("click", function () {
+    "use strict";
+    if ($("#times").prop("checked")) {
+        em.addOperators("x");
+    } else {
+        em.removeOperator("x");
+    }
+    em.fillNumberFields();
 });
